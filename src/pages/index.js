ec02b3e745c953806cc573360effec155dbbb57a
Dinuja AKA D3 <46810937+dilmith99@users.noreply.github.com>
2024-03-14 03:54:10 +0300

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,11 +41,11 @@ export default function Home() {
             </div>
             <div className="flex w-1/2 flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
-                text="Turning vision into reality with code and design."
+                text="Discover Angampora: The Art of Fearless Combat."
                 className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
-              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+              Explore Angampora, the ancient Sri Lankan martial art that blends tradition, strength, and fearlessness. Uncover its lethal techniques, rich heritage, and warrior spirit.
               </p>
               <div className="mt-2 flex items-center self-start lg:self-center">
                 <Link
@@ -61,7 +61,7 @@ export default function Home() {
              `}
                   download
                 >
-                  Resume <LinkArrow className="ml-1 !w-6 md:!w-4" />
+                  Read <LinkArrow className="ml-1 !w-6 md:!w-4" />
                 </Link>
 
                 <Link
